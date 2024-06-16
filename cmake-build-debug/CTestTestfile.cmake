1c510d8e8fb181278432a8ba6599433f0a39fb7c
47813 <vanguard0956@gmail.com>
2024-06-16 13:08:14 +0900

    Stop tracking cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dongeonlee/doonoon-main
-# Build directory: /Users/dongeonlee/doonoon-main/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
